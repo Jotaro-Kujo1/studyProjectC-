775dd20aa2c575e63613937b85ef1c4d8fc83de6
Vadim <lebedev.vada@gmail.com>
2022-03-20 23:09:48 +0300

    Реализован паттерн MVVM

--- MVVMDemo/AdditionOfNumbers.xaml.cs
+++ MVVMDemo/AdditionOfNumbers.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace MVVMDemo
 {
@@ -7,6 +9,14 @@ namespace MVVMDemo
         public AdditionOfNumbers()
         {
             InitializeComponent();
+        }
+
+        public void sum_Click(object sender, RoutedEventArgs e)
+        {
+            MainVM vm = new MainVM();
+            vm.Number1 = Convert.ToInt32(txtBoxNumA.Text);
+            vm.Number2 = Convert.ToInt32(txtBoxNumB.Text);
+            txtBoxRes.AppendText(Convert.ToString(vm.Number3));
         }
     }
 }
---
+++ MVVMDemo/MathSum.cs
@@ -0,0 +1,8 @@
+﻿namespace MVVMDemo
+{
+    //Model
+    public static class MathSum
+    {
+        public static int GetSum(int a, int b) => a + b;
+    }
+}
