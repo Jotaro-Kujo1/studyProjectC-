0da57f0cfcd195a90985f3d92d0f1bef80781553
Vadim <lebedev.vada@gmail.com>
2022-03-29 20:57:54 +0300

    Работа доделана

--- MVVMDemo/Convertors/StringToInt.cs
+++ MVVMDemo/Convertors/StringToInt.cs
@@ -9,15 +9,7 @@ namespace MVVMDemo.Convertors
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string str = (string) value;
-            int res;
-            if (int.TryParse(str, out res))
-            {
-                return res = Int32.Parse(str);
-            }
-            else
-            {
-                return str.Length;
-            }
+            return str.Length.ToString();
         }
     }
 }
--- MVVMDemo/MathSum.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MVVMDemo
-{
-    //Model
-    public static class MathSum
-    {
-        public static int GetSum(int a, int b) => a + b;
-    }
-}
