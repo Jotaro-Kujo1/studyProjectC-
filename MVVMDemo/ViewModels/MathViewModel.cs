1787f093dd3cf271fbba72b60437ea841b5ad170
Vadim <lebedev.vada@gmail.com>
2022-04-14 20:45:38 +0300

    добавлены новые кнопки

--- MVVMDemo/ViewModels/MathViewModel.cs
+++ MVVMDemo/ViewModels/MathViewModel.cs
@@ -16,11 +16,9 @@ namespace MVVMDemo.ViewModels
 
         public MathViewModel()
         {
-            //таким нехитрым способом мы пробрасываем изменившиеся свойства модели во View
             _model.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
             
             AddCommand = new DelegateCommand<string>(str => {
-                //проверка на валидность ввода - обязанность VM
                 int ival;
                 if (int.TryParse(str, out ival)) _model.AddValue(ival);
             });
@@ -30,8 +28,8 @@ namespace MVVMDemo.ViewModels
         }
 
 
-        public ICommand AddCommand { get; }
-        //public DelegateCommand<string> AddCommand { get; }
+        //public ICommand AddCommand { get; }
+        public DelegateCommand<string> AddCommand { get; }
         public DelegateCommand<int?> RemoveCommand { get; }
         public int Sum => _model.Sum;
         public ReadOnlyObservableCollection<int> MyValues => _model.MyPublicValues;
